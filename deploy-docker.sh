4cd74494c81811b8e9e7ab2db1a466f7d9378fbf
AkshayHere <akshymohn@gmail.com>
2025-10-26 19:04:56 +0800

    Added docker deploy script

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DOCKER_TAG=$(git rev-parse --short HEAD)
+# DOCKER_TAG=$(git rev-parse --short HEAD)
+DOCKER_TAG=$(date +%Y%m%d-%H%M%S)
 echo "Latest hash for tag: $DOCKER_TAG"
-docker build -t akshayhere/teachers-portal-app:$DOCKER_TAG --build-arg DOCKER_TAG=$DOCKER_TAG .
-echo "Build locally successful"
-docker push akshayhere/teachers-portal-mysql:$DOCKER_TAG
-docker tag akshayhere/teachers-portal-mysql:$DOCKER_TAG akshayhere/teachers-portal-mysql:latest
-docker push akshayhere/teachers-portal-mysql:latest
-echo "Build deployed to docker hub"
+docker build -t teachers-portal-app:$DOCKER_TAG .
+docker tag teachers-portal-app:$DOCKER_TAG akshayhere/teachers-portal-app:$DOCKER_TAG
+docker push akshayhere/teachers-portal-app:$DOCKER_TAG
+docker tag akshayhere/teachers-portal-app:$DOCKER_TAG akshayhere/teachers-portal-app:latest
+docker push akshayhere/teachers-portal-app:latest
