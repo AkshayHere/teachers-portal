35cf3858c8de8334d75a0c74386010ac7ea1301c
Akshay <akshay.mohan@hlas.com.sg>
2025-10-31 17:37:27 +0800

    Patched swagger config Added restart logic

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,9 +3,17 @@ import swaggerJsdoc from "swagger-jsdoc";
 const options = {
   definition: {
     openapi: "3.0.0",
-    info: { title: "Teacher's Portal API", version: "1.0.0" },
+    info: { title: "Teacher's Portal API", version: "1.2.0" },
   },
-  apis: ["src/routes/*.ts"],
+  servers: [
+    {
+      url: "http://localhost:3000",
+    },
+  ],
+  apis: [
+    "./src/routes/*.ts",
+    "./dist/src/routes/*.js",
+  ],
 };
 
 export const swaggerSpec = swaggerJsdoc(options);
