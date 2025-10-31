429d8d17ea2ce2eb0bc154e28f4e8241ad592e2c
Akshay <akshay.mohan@hlas.com.sg>
2025-10-31 16:38:19 +0800

    Added docs with Swagger

---
+++ src/swagger.ts
@@ -0,0 +1,11 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: { title: "Teacher's Portal API", version: "1.0.0" },
+  },
+  apis: ["src/routes/*.ts"],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
